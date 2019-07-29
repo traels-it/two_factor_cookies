262272adcead84c02af7b2ebb062da62a6d2a899
Nicolai Bach Woller <woller@traels.it>
2019-07-29 14:15:18 +0200

    New feature. Make the tests from sessions_controller pass.

--- test/dummy/app/controllers/sessions_controller.rb
+++ test/dummy/app/controllers/sessions_controller.rb
@@ -1,8 +1,13 @@
 class SessionsController < ApplicationController
   def new
-    if user = User.logon(params['user'], params['pass'])
-      session[:user_id] = user.to_param
-      redirect_to root_url, notice: t('session.logged_in')
+    if (user = User.logon(params[:username], params[:password]))
+      if two_factor_authenticate?(user)
+        session[:unauthenticated_user_id] = user.id
+        redirect_to two_factor_cookies.show_two_factor_authentication_path
+      else
+        session[:user_id] = user.to_param
+        redirect_to root_url, notice: t('session.logged_in')
+      end
     else
       session[:user_id] = nil
       redirect_to root_url, notice: t('session.invalid_user_or_pass')
@@ -13,4 +18,23 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: t('session.logged_out')
   end
+
+  private
+
+    def two_factor_authenticate?(user)
+      return false unless user
+      return false unless user.enabled_two_factor? && user.confirmed_phone_number?
+      return false if two_factor_approved?(user)
+
+      true
+    end
+
+    def two_factor_approved?(user)
+      return false if cookies.encrypted[:mfa].nil?
+
+      parsed_cookies = JSON.parse(cookies.encrypted[:mfa]).symbolize_keys
+      return false if parsed_cookies[:user_name] != user.login_mail
+
+      parsed_cookies[:approved]
+    end
 end
--- test/dummy/test/controllers/sessions_controller_test.rb
+++ test/dummy/test/controllers/sessions_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include TwoFactorCookies::Engine.routes.url_helpers
+
+  let(:user) { User.create(username: 'username', password: 'password', phone: '+4512341234', confirmed_phone_number: false, enabled_two_factor: false) }
+
+  describe '2fa not approved' do
+    before do
+      TwoFactorCookies::TextMessage.stubs(:send)
+      user.update(confirmed_phone_number: true, enabled_two_factor: true)
+    end
+
+    it 'redirects to two_factor_authentication controller' do
+      post login_url, params: { username: user.username, password: user.password }
+
+      assert_redirected_to two_factor_cookies.show_two_factor_authentication_path
+    end
+  end
+
+  describe '2fa enabled, but number is not confirmed' do
+    it 'does not do 2fa' do
+      user.update(enabled_two_factor: true)
+
+      post login_url, params: { username: user.username, password: user.password }
+
+      assert_redirected_to root_url
+    end
+  end
 end
